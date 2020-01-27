bffa4bdc54762b145493b8da33842519f9744f64
ipfs2018 <bratick@mail.ru>
2020-01-27 17:32:43 +0300

    Task #4 completed.

--- lsn2/l2_task4.py
+++ lsn2/l2_task4.py
@@ -3,3 +3,12 @@
  Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
  Если в слово длинное, выводить только первые 10 букв в слове. 
 """
+user_input = input('Enter a sentence:')
+
+user_list = list(user_input.split())
+
+for num, item in enumerate(user_list):
+    if len(item) > 10:
+        print(num, '%.10s' % item)
+    else:
+        print(num, item)
