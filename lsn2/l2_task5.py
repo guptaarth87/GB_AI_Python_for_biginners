1aa4fb1dea41e56da8b777d1c32b5b6439d178c2
ipfs2018 <bratick@mail.ru>
2020-01-29 02:32:57 +0300

    Task #5 completed.

--- lsn2/l2_task5.py
+++ lsn2/l2_task5.py
@@ -1,7 +1,8 @@
 """
 Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел.
  У пользователя необходимо запрашивать новый элемент рейтинга.
- Если в рейтинге существуют элементы с одинаковыми значениями, то новый элемент с тем же значением должен разместиться после них.
+ Если в рейтинге существуют элементы с одинаковыми значениями,
+  то новый элемент с тем же значением должен разместиться после них.
 
 Подсказка. Например, набор натуральных чисел: 7, 5, 3, 3, 2. 
 Пользователь ввел число 3. Результат: 7, 5, 3, 3, 3, 2.
@@ -10,3 +11,22 @@
 Набор натуральных чисел можно задать непосредственно в коде, например, my_list = [7, 5, 3, 3, 2].
 
 """
+my_list = [10, 7, 2, 3, 5, 3]
+my_list.sort(reverse=True)
+
+print(my_list)
+while True:
+    user_input = int(input('>>'))
+    if user_input in my_list:
+        x = my_list.index(user_input)
+        y = my_list.count(user_input)
+        my_list.insert((x + y), user_input)
+    elif user_input > my_list[0]:
+        my_list.insert(0, user_input)
+    elif user_input < my_list[-1]:
+        my_list.insert(len(my_list), user_input)
+    else:
+        for item in range(len(my_list) - 1, -1, -1):
+            if my_list[item] < user_input <= my_list[item - 1]:
+                my_list.insert(item, user_input)
+    print(my_list)
