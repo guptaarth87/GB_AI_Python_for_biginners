1f6758eea3fa6c5fccceb11e1fac9c05e2e9a20d
ipfs2018 <bratick@mail.ru>
2020-02-01 14:45:02 +0300

    Task #1 completed.

--- lsn3/l3_task1.py
+++ lsn3/l3_task1.py
@@ -2,3 +2,17 @@
  Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
 
+def my_div(a,b):
+    try:
+        return float(a)/float(b)
+    except:
+        return "Something is wrong. Maybe it is diviion by Zero or symbol's input.\n"
+
+
+while True:
+    print('Let try to divide A by B.')
+    a=input('Enter A:')
+    b=input('Enter B:')
+
+    div_result=my_div(a,b)
+    print(div_result)
