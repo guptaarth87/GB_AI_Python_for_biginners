5e0fd723a4f9b826fde754528f9a3a4edda1a308
ipfs2018 <bratick@mail.ru>
2020-02-01 14:58:36 +0300

    Task #1 completed. Correct return.

--- lsn3/l3_task1.py
+++ lsn3/l3_task1.py
@@ -6,13 +6,14 @@ def my_div(a,b):
     try:
         return float(a)/float(b)
     except:
-        return "Something is wrong. Maybe it is diviion by Zero or symbol's input.\n"
+        print("Something is wrong. Maybe it is diviion by Zero or symbol's input.")
+        return None
 
 
 while True:
-    print('Let try to divide A by B.')
+    print('\nLet try to divide A by B.')
     a=input('Enter A:')
     b=input('Enter B:')
 
     div_result=my_div(a,b)
-    print(div_result)
+    print(f'A/B={div_result}')
