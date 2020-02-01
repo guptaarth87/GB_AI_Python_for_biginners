5280ef941eb9ade40de24756a86ad3f60403f1aa
ipfs2018 <bratick@mail.ru>
2020-02-01 15:30:12 +0300

    Task #3 completed.

--- lsn3/l3_task3.py
+++ lsn3/l3_task3.py
@@ -1,4 +1,27 @@
 """
 Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
-
 """
+
+
+def sum_2_max(a, b, c):
+    try:
+        list = [int(a), int(b), int(c)]
+
+        max1 = max(list)
+        list.remove(max1)
+        max2 = max(list)
+
+        return max1 + max2
+    except:
+        print('Wrong data.')
+        return None
+
+
+while True:
+    print('Enter 3 number:')
+    a = input('A=')
+    b = input('B=')
+    c = input('C=')
+
+    result=sum_2_max(a, b, c)
+    print(f'Result is {result}.\n')
