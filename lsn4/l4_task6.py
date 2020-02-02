dded325ec13668edb69b1ffa14409b1a5bdca8ff
ipfs2018 <bratick@mail.ru>
2020-02-03 00:22:10 +0300

    Task #6 completed.

--- lsn4/l4_task6.py
+++ lsn4/l4_task6.py
@@ -7,3 +7,37 @@
 Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
 Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
 """
+
+from sys import argv
+from itertools import count, cycle
+
+
+def test_A(x, y):
+    for num in count(int(x)):
+        if num < int(y):
+            print(num)
+        else:
+            return 'Done test A.'
+
+
+def test_B(x, y):
+    i = 0
+    for item in cycle(x):
+        i += 1
+        if i < int(y):
+            print(item)
+        else:
+            return 'Done test B.'
+
+
+# list1=[2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+type_iter, long_iter, max_iter = argv[1:]
+print(argv[1:])
+
+if type_iter == 'A':
+    print(test_A(long_iter, max_iter))
+elif type_iter == 'B':
+    print(test_B(long_iter, max_iter))
+else:
+    print('You should choose option A or B.')
