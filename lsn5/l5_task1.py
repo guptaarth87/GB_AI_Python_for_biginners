99a003e78e837d0f7ae9ed622848d50ed2b70b8a
ipfs2018 <bratick@mail.ru>
2020-02-08 22:45:38 +0300

    Task #1 completed.

--- lsn5/l5_task1.py
+++ lsn5/l5_task1.py
@@ -3,3 +3,13 @@
  Об окончании ввода данных свидетельствует пустая строка.
 
 """
+user_in=None
+with open('data/task1.data', 'w') as f:
+    while True:
+        user_in = input()+'\n'
+        if user_in=='\n':
+            break
+        else:
+            f.write(user_in)
+
+print('Finished.')
