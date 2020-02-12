f91f0b00f31f48b36f8b4aab82eb1bd5c7a88f88
ipfs2018 <bratick@mail.ru>
2020-02-12 07:53:59 +0300

    Task #3 ~.

--- lsn6/l6_task3.py
+++ lsn6/l6_task3.py
@@ -9,3 +9,54 @@
 Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения
  атрибутов, вызвать методы экземпляров).
 """
+
+
+class Worker:
+
+    name = str()
+    surname = str()
+    position = str()
+    _income = dict({"wage": int(), "bonus": int()})
+
+    def __init__(self, name, surname, position, wage):
+        self.name = name
+    #     self.surname = surname
+    #     self.position = position
+    #     self._income.update({"wage": int(wage)})
+    #     print(self.name, self.surname, self.position, self._income['wage'])
+
+
+class Position(Worker):
+
+    def __init__(self,name,surname,position,wage):
+        super().__init__(name,surname,position,wage)
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income.update({"wage": int(wage)})
+        # self._income= wage
+        print(self.name, self.surname, self.position, self._income['wage'])
+        # print(self.name, self.surname, self.position, self._income)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self, b):
+        self._income['bonus'] = b
+        return int(self._income['wage']) + int(self._income['bonus'])
+        # self.bonus = b
+        # return int(self._income) + int(self.bonus)
+
+
+wrk1 = Position('John', 'Deer', 'driver', '1')
+wrk2 = Position('Ringo', 'Star', 'musician', '9000')
+print(wrk1._income)
+print(wrk2._income)
+
+# print(wrk1.name, wrk1.surname, wrk1.position, wrk1._income['wage'])
+print(wrk1.get_full_name())
+print(wrk1.get_total_income(500))
+
+# print(wrk2.name, wrk2.surname, wrk2.position, wrk2._income['wage'])
+print(wrk2.get_full_name())
+print(wrk2.get_total_income(250))
